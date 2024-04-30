12c5e7c2c4a24430ddfa8ef7d83f71b9bfdb100d
Robi <117577402+RobiLeskur@users.noreply.github.com>
2024-04-30 19:43:19 +0200

    newActivityData is ready to be sent, just need to ffix the select conteolled problem

--- src/components/activities/NewActivityPopup.tsx
+++ src/components/activities/NewActivityPopup.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { CloseButton } from 'react-bootstrap';
 import styles from './activities.module.css';
 import Button from 'react-bootstrap/Button';
@@ -12,6 +12,14 @@ import Volunteer from '../../interfaces/VolunteerInterface';
 function ActivitiesCard({ toggleNewActivityPopup }: { toggleNewActivityPopup: () => void }) {
     const [organizations, setOrganizations] = useState([] as Organization[]);
     const [volunteers, setVolunteers] = useState([] as Volunteer[]);
+    const [newActivityData, setNewActivityData] = useState({
+        ime: "",
+        description: "",
+        date: "",
+        location: "",
+        organization: "",
+        volunteers: [] as string[]
+    });
 
     useEffect(() => {
         axios
@@ -27,6 +35,30 @@ function ActivitiesCard({ toggleNewActivityPopup }: { toggleNewActivityPopup: ()
             .catch(err => console.log(err));
     }, []);
 
+    function inputChange(event: any) {
+        const { name, value } = event.target;
+        setNewActivityData({ ...newActivityData, [name]: value });
+
+        if (name === 'date') {
+            // Format the date to "dd.mm.yyyy"
+            const [year, month, day] = value.split('-');
+            const formattedDate = `${day}.${month}.${year}`;
+            setNewActivityData({ ...newActivityData, [name]: formattedDate });
+        } else {
+            setNewActivityData({ ...newActivityData, [name]: value });
+        }
+    
+
+        console.log(newActivityData);
+
+    }
+
+
+
+    function handleVolunteersChange(event: React.ChangeEvent<HTMLSelectElement>) {
+        const selectedVolunteers = Array.from(event.target.selectedOptions).map(option => option.value);
+        setNewActivityData({ ...newActivityData, volunteers: selectedVolunteers });
+    }
 
     return (
         <div className={styles.popup}>
@@ -35,44 +67,46 @@ function ActivitiesCard({ toggleNewActivityPopup }: { toggleNewActivityPopup: ()
                 <Form>
                     <h2 style={{ padding: '10px' }}>Dodaj aktivnost</h2>
                     <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Control type="text" placeholder="Ime" />
+                        <Form.Group as={Col}>
+                            <Form.Control required type="text" name='ime' value={newActivityData.ime} onChange={inputChange} placeholder="Ime" />
                         </Form.Group>
-                        <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Control type="date" placeholder="Ime" />
-                        </Form.Group>
-                    </Row>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Control type="text" placeholder="Opis" />
+                        <Form.Group as={Col}>
+                            <Form.Control required type="date" name='date' onChange={inputChange} />
                         </Form.Group>
                     </Row>
                     <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridEmail">
-                            <Form.Control type="text" placeholder="Lokacija" />
+                        <Form.Group as={Col}>
+                            <Form.Control required type="text" name='description' value={newActivityData.description} onChange={inputChange} placeholder="Opis" />
                         </Form.Group>
                     </Row>
                     <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>Organizacija</Form.Label>
-                        <Form.Select defaultValue="Organizacija">
-                           {organizations.map((organization: Organization) => (
-                                <option key={organization.id} value={organization.id}>{organization.name}</option>
-                            ))}     
-                        </Form.Select>
-                    </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Control required type="text" name='location' value={newActivityData.location} onChange={inputChange} placeholder="Lokacija" />
+                        </Form.Group>
                     </Row>
                     <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>Volonteri: </Form.Label>
-                        <Form.Select multiple defaultValue="Organizacija">
-                           {volunteers.map((volunteer: Volunteer) => (
-                                <option key={volunteer.id} value={volunteer.id}>{volunteer.first_name}</option>
-                            ))} 
-                        </Form.Select>
-                    </Form.Group>
+                        <Form.Group as={Col} required controlId="formGridState">
+                            <Form.Label>Organizacija</Form.Label>
+                            <Form.Select name='organization' value={newActivityData.organization} onChange={inputChange} defaultValue="">
+                                {organizations.map((organization: Organization) => (
+                                    <option key={organization.id} value={organization.id}>{organization.name}</option>
+                                ))}
+                            </Form.Select>
+                        </Form.Group>
                     </Row>
-
+                    <Row className="mb-3">
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>Volonteri: </Form.Label>
+                            <Form.Select multiple name="volunteers" value={newActivityData.volunteers} onChange={handleVolunteersChange}>
+                                {volunteers.map((volunteer: Volunteer) => (
+                                    <option key={volunteer.id} value={volunteer.id}>{volunteer.first_name}</option>
+                                ))}
+                            </Form.Select>
+                        </Form.Group>
+                    </Row>
+                    <Button variant="success" type="submit">
+                        Dodaj
+                    </Button>
                 </Form>
             </div>
         </div>
